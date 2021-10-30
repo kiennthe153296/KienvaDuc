d3fb19a84770d878436281e1cca0f0532bbb747b
Ducndthe153148 <Ducndthe153148@fpt.edu.vn>
2021-10-30 12:02:51 +0700

    Push first

---
+++ LibraryAsp/LibraryAsp/Controllers/PublisherController.cs
@@ -0,0 +1,39 @@
+﻿using LibraryAsp.Dao;
+using LibraryAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAsp.Controllers
+{
+    public class PublisherController : Controller
+    {
+        PublisherDao publisher = new PublisherDao();
+        // GET: Publisher
+        public ActionResult Index(string msg)
+        {
+            ViewBag.Msg = msg;
+            ViewBag.List = publisher.getAll();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(FormCollection form)
+        {
+            Publisher pub = new Publisher();
+            pub.name = form["name"];
+            publisher.add(pub);
+            return RedirectToAction("Index", new { msg = "1" });
+        }
+     /*   [HttpPost]
+        public ActionResult Update(FormCollection form)
+        {
+            Publisher pub = new Publisher();
+            pub.id_publisher =Int32.Parse(form["id"]);
+            pub.name = form["name"];
+            publisher.edit(pub);
+            return RedirectToAction("Index", new { msg = "1" });
+        }*/
+    }
+}
